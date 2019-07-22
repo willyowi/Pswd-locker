5915c492fc9ba340ac357970a80e75add79f815b
willyowi <wilsonowino1@gmail.com>
2019-07-22 10:29:02 +0300

    user-test.py onset

--- user_test.py
+++ user_test.py
@@ -1,24 +1,24 @@
 import unittest
 from  user import User
 
-class TestUser(unittest.TestCase):
-    def setUp(self):
-        self.new_user = User("Collins","Kariuki","collins@gmail.com",)
-    def test1(self):
-        self.assertEqual(self.new_user.f_name,"Collins")
-        self.assertEqual(self.new_user.m_name,"Kariuki")
-        self.assertEqual(self.new_user.e_mail,"collins@gmail.com")
-    def test_save_user(self):
-        self.new_user.save_user()
-
-    def tearDown(self):
-        User.userlist = []
-    def test_delete_user(self):
-        self.new_user.save_user()
-        test_data= User("ariana","gathoni","ariana@gmail.com")
-        test_data.save_user()
-        self.assertEqual(len(User.user_list),2)
-    def test_display_user(self):
-        self.assertEqual(User.display_users(),User.user_list)    
-if __name__ == '__main__':
-    unittest.main()
+# class TestUser(unittest.TestCase):
+#     def setUp(self):
+#         self.new_user = User("Collins","Kariuki","collins@gmail.com",)
+#     def test1(self):
+#         self.assertEqual(self.new_user.f_name,"Collins")
+#         self.assertEqual(self.new_user.m_name,"Kariuki")
+#         self.assertEqual(self.new_user.e_mail,"collins@gmail.com")
+#     def test_save_user(self):
+#         self.new_user.save_user()
+#
+#     def tearDown(self):
+#         User.userlist = []
+#     def test_delete_user(self):
+#         self.new_user.save_user()
+#         test_data= User("ariana","gathoni","ariana@gmail.com")
+#         test_data.save_user()
+#         self.assertEqual(len(User.user_list),2)
+#     def test_display_user(self):
+#         self.assertEqual(User.display_users(),User.user_list)
+# if __name__ == '__main__':
+#     unittest.main()
