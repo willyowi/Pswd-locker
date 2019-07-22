927d360740bf58c703378355d7f7779d53764455
willyowi <wilsonowino1@gmail.com>
2019-07-22 11:11:23 +0300

    add add loopthrough run.py

--- run.py
+++ run.py
@@ -52,70 +52,70 @@
              print ("what is your facebook password?..")
              print(" ")
              face_bookp =input()
-#             print("what is your email password?..")
-#             print(" ")
-#             e_mailp= input()
-#             save_account(create_account(f_name,m_name,e_mail))
-#             print('\n')
-#             save_credentials(create_credentials (face_bookp,e_mailp))
-#             print('\n')
-#             print("-" * 156)
-#             print(f"New Account  {f_name } { m_name} { face_bookp } has been created")
-#             print('\n')
-#         elif short_code =='dac':
-#             if display_users():
-#                 print(" ")
-#                 print("The user name")
-#                 print(" ")
-#                 print('\n')
-#                 for user in display_users():
-#                     print(f"{user.f_name}{user.m_name}")
-#                 for credentials in display_creds():
-#                     print (f"{face_bookp}")
-#                     print(" ")
-#
-#             else:
-#                     print('\n')
-#                     print("-" * 156)
-#                     print(" ")
-#                     print("                         PLEASE CREATE AN ACCOUNT ")
-#                     print("                    You have not created an account yet :( ")
-#                     print(" ")
-#         elif  short_code == 'gs':
-#             print(" ")
-#             print(" ")
-#             print("TO GENERATE A PASSWORD ADD IN YOUR FIRST NAME AND FACEBOOK BELOW!!")
-#             print(" ")
-#             list_of_inputs = [c for c in input()]
-#
-#             # list_of_inputs= list(list_of_inputs)
-#             list_of_inputs.reverse()
-#
-#
-#
-#             print (list_of_inputs)
-#
-#
-#
-#
-#
-#
-#         elif short_code == "ex":
-#             print("-" * 156)
-#             print(" ")
-#             print("                        THAX FOR DROPING IN!")
-#             print("                           Bye... Bye...")
-#             print(" ")
-#             print("-" * 156)
-#             break
-#         else:
-#             print("-" * 156)
-#             print(" ")
-#             print("                              RETRY!!")
-#             print(" ")
-#             print("                Please Select One Of The Options Provided")
-#             print(" ")
-#
-# if __name__ == '__main__':
-#
-#     main()
+             print("what is your email password?..")
+             print(" ")
+             e_mailp= input()
+             save_account(create_account(f_name,m_name,e_mail))
+            print('\n')
+            save_credentials(create_credentials (face_bookp,e_mailp))
+            print('\n')
+            print("-" * 156)
+            print(f"New Account  {f_name } { m_name} { face_bookp } has been created")
+            print('\n')
+        elif short_code =='dac':
+            if display_users():
+                print(" ")
+                print("The user name")
+                print(" ")
+                print('\n')
+                for user in display_users():
+                    print(f"{user.f_name}{user.m_name}")
+                for credentials in display_creds():
+                    print (f"{face_bookp}")
+                    print(" ")
+
+            else:
+                    print('\n')
+                    print("-" * 156)
+                    print(" ")
+                    print("                         PLEASE CREATE AN ACCOUNT ")
+                    print("                    You have not created an account yet :( ")
+                    print(" ")
+        elif  short_code == 'gs':
+            print(" ")
+            print(" ")
+            print("TO GENERATE A PASSWORD ADD IN YOUR FIRST NAME AND FACEBOOK BELOW!!")
+            print(" ")
+            list_of_inputs = [c for c in input()]
+
+            # list_of_inputs= list(list_of_inputs)
+            list_of_inputs.reverse()
+
+
+
+            print (list_of_inputs)
+
+
+
+
+
+
+        elif short_code == "ex":
+            print("-" * 156)
+            print(" ")
+            print("                        THAX FOR DROPING IN!")
+            print("                           Bye... Bye...")
+            print(" ")
+            print("-" * 156)
+            break
+        else:
+            print("-" * 156)
+            print(" ")
+            print("                              RETRY!!")
+            print(" ")
+            print("                Please Select One Of The Options Provided")
+            print(" ")
+
+if __name__ == '__main__':
+
+    main()
