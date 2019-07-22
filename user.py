aa6d89468468ec92db13b32f501bcfd76efe620f
willyowi <wilsonowino1@gmail.com>
2019-07-22 10:43:47 +0300

    add  and update user.py

--- user.py
+++ user.py
@@ -1,9 +1,9 @@
 class User:
     user_list = []
 
-    # def __init__ (self,f_name,m_name,e_mail):
-    #
-    #   self.f_name = f_name
+     def __init__ (self,f_name,m_name,e_mail):
+
+       self.f_name = f_name
     #   self.m_name = m_name
     #   self.e_mail = e_mail
     #
