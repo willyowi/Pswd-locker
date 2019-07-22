8d7042aaeff26292dbb1ff134f0c3af60bc14087
willyowi <wilsonowino1@gmail.com>
2019-07-22 10:11:07 +0300

    add  def setUp and test and save info

--- credentials_test.py
+++ credentials_test.py
@@ -2,12 +2,12 @@ import unittest
 from credentials import Info
 
  class TestInfo(unittest.TestCase):
-#     def setUp(self):
-#         self.new_info =Info("collins.kariuki","kariuki.collins")
-#     def test_init(self):
-#         self.assertEqual(self.new_info.face_bookp,"collins.kariuki")
-#         self.assertEqual(self.new_info.email_p,"kariuki.collins")
-#     def test_save_info(self):
+     def setUp(self):
+         self.new_info =Info("collins.kariuki","kariuki.collins")
+     def test_init(self):
+         self.assertEqual(self.new_info.face_bookp,"collins.kariuki")
+         self.assertEqual(self.new_info.email_p,"kariuki.collins")
+     def test_save_info(self):
 #         '''
 #         to test if user info is saved
 #         '''
