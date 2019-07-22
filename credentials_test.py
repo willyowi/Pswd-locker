594468375c84f3a64559a2514d3923938b7e1628
willyowi <wilsonowino1@gmail.com>
2019-07-22 10:13:48 +0300

    add display creds

--- credentials_test.py
+++ credentials_test.py
@@ -11,18 +11,18 @@ from credentials import Info
          '''
          to test if user info is saved
          '''
-#         self.new_info.save_info()
-#         self.assertEqual(len(Info.info_list),1)
-#     def tearDown(self):
-#         Info.info_list = []
-#     def test_delete_info(self):
-#         self.new_info.save_info()
-#         test_info = Info("agens.89","agnes.89")
-#         test_info.save_info()
-#         test_info.delete_info()
-#         self.assertEqual(len(Info.info_list),1)
-#     def test_display_creds(self):
-#         self.assertEqual(Info.display_info(),Info.info)
-#
-# if __name__ == '__main__':
-#     unittest.main()
+         self.new_info.save_info()
+         self.assertEqual(len(Info.info_list),1)
+     def tearDown(self):
+         Info.info_list = []
+     def test_delete_info(self):
+         self.new_info.save_info()
+         test_info = Info("agens.89","agnes.89")
+         test_info.save_info()
+         test_info.delete_info()
+         self.assertEqual(len(Info.info_list),1)
+     def test_display_creds(self):
+         self.assertEqual(Info.display_info(),Info.info)
+
+ if __name__ == '__main__':
+     unittest.main()
