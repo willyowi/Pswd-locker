dff654c52ff9a32916be0aa77ce40fa17e544991
willyowi <wilsonowino1@gmail.com>
2019-07-22 11:02:22 +0300

    add create account

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 # #!/usr/bin/env python3.6
  from user import User
  from credentials import Info
-#
-# def create_account(f_name,m_name,e_mail):
-#     new_user = User(f_name,m_name,e_mail)
-#     return new_user
+
+ def create_account(f_name,m_name,e_mail):
+     new_user = User(f_name,m_name,e_mail)
+     return new_user
 # def create_credentials(face_bookp,e_mailp):
 #     new_cred = Info(face_bookp,e_mailp)
 #     return new_cred
